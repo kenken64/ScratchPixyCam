97e12539e6dc3af43ac77a3f314b2f578308cfb2
kenken64 <bunnyppl@hotmail.com>
2016-04-20 18:23:08 -0400

    first commit

---
+++ PixyCMUCamExtension.js
@@ -0,0 +1,297 @@
+/*
+ *This program is free software: you can redistribute it and/or modify
+ *it under the terms of the GNU General Public License as published by
+ *the Free Software Foundation, either version 3 of the License, or
+ *(at your option) any later version.
+ *
+ *This program is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU General Public License for more details.
+ *
+ *You should have received a copy of the GNU General Public License
+ *along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+(function(ext) {
+  var PIN_MODE = 0xF4,
+    REPORT_DIGITAL = 0xD0,
+    REPORT_ANALOG = 0xC0,
+    DIGITAL_MESSAGE = 0x90,
+    START_SYSEX = 0xF0,
+    END_SYSEX = 0xF7,
+    QUERY_FIRMWARE = 0x79,
+    REPORT_VERSION = 0xF9,
+    ANALOG_MESSAGE = 0xE0,
+    ANALOG_MAPPING_QUERY = 0x69,
+    ANALOG_MAPPING_RESPONSE = 0x6A,
+    CAPABILITY_QUERY = 0x6B,
+    CAPABILITY_RESPONSE = 0x6C;
+
+  var INPUT = 0x00,
+    OUTPUT = 0x01,
+    ANALOG = 0x02,
+    PWM = 0x03,
+    SERVO = 0x04,
+    SHIFT = 0x05,
+    I2C = 0x06,
+    ONEWIRE = 0x07,
+    STEPPER = 0x08,
+    ENCODER = 0x09,
+    SERIAL = 0x0A,
+    PULLUP = 0x0B,
+    IGNORE = 0x7F,
+    TOTAL_PIN_MODES = 13;
+
+  var LOW = 0,
+    HIGH = 1;
+
+  var MAX_DATA_BYTES = 4096;
+  var MAX_PINS = 128;
+
+  var parsingSysex = false,
+    waitForData = 0,
+    executeMultiByteCommand = 0,
+    multiByteChannel = 0,
+    sysexBytesRead = 0,
+    storedInputData = new Uint8Array(MAX_DATA_BYTES);
+
+  var digitalOutputData = new Uint8Array(16),
+    digitalInputData = new Uint8Array(16),
+    analogInputData = new Uint16Array(16);
+
+  var analogChannel = new Uint8Array(MAX_PINS);
+  var pinModes = [];
+  for (var i = 0; i < TOTAL_PIN_MODES; i++) pinModes[i] = [];
+
+  var majorVersion = 0,
+    minorVersion = 0;
+
+  var connected = false;
+  var notifyConnection = false;
+  var device = null;
+  var inputData = null;
+
+  // TEMPORARY WORKAROUND
+  // Since _deviceRemoved is not used with Serial devices
+  // ping device regularly to check connection
+  var pinging = false;
+  var pingCount = 0;
+  var pinger = null;
+
+  var hwList = new HWList();
+
+  function HWList() {
+    this.devices = [];
+
+    this.add = function(dev, pin) {
+      var device = this.search(dev);
+      if (!device) {
+        device = {name: dev, pin: pin, val: 0};
+        this.devices.push(device);
+      } else {
+        device.pin = pin;
+        device.val = 0;
+      }
+    };
+
+    this.search = function(dev) {
+      for (var i=0; i<this.devices.length; i++) {
+        if (this.devices[i].name === dev)
+          return this.devices[i];
+      }
+      return null;
+    };
+  }
+
+  function init() {
+
+    for (var i = 0; i < 16; i++) {
+      var output = new Uint8Array([REPORT_DIGITAL | i, 0x01]);
+      device.send(output.buffer);
+    }
+
+    queryCapabilities();
+
+    // TEMPORARY WORKAROUND
+    // Since _deviceRemoved is not used with Serial devices
+    // ping device regularly to check connection
+    pinger = setInterval(function() {
+      if (pinging) {
+        if (++pingCount > 6) {
+          clearInterval(pinger);
+          pinger = null;
+          connected = false;
+          if (device) device.close();
+          device = null;
+          return;
+        }
+      } else {
+        if (!device) {
+          clearInterval(pinger);
+          pinger = null;
+          return;
+        }
+        //queryFirmware();
+        pinging = true;
+      }
+    }, 100);
+  }
+
+  function hasCapability(pin, mode) {
+    if (pinModes[mode].indexOf(pin) > -1)
+      return true;
+    else
+      return false;
+  }
+
+  function UInt8ArrayToString(uInt8Array)
+  {
+    var s = "[";
+    for(var i = 0; i < uInt8Array.byteLength; i++)
+    {
+        if( i > 0 )
+            s += ", ";
+        s += uInt8Array[i];
+    }
+    s += "]";
+    return s;
+  }        
+
+  function queryFirmware() {
+    var output = new Uint8Array([START_SYSEX, QUERY_FIRMWARE, END_SYSEX]);
+    device.send(output.buffer);
+  }
+
+  function queryCapabilities() {
+    console.log('Querying ' + device.id + ' capabilities');
+    var msg = new Uint8Array([
+        START_SYSEX, CAPABILITY_QUERY, END_SYSEX]);
+    device.send(msg.buffer);
+  }
+
+  function queryAnalogMapping() {
+    console.log('Querying ' + device.id + ' analog mapping');
+    var msg = new Uint8Array([
+        START_SYSEX, ANALOG_MAPPING_QUERY, END_SYSEX]);
+    device.send(msg.buffer);
+  }
+
+  function setVersion(major, minor) {
+    majorVersion = major;
+    minorVersion = minor;
+  }
+
+  function ab2str(buf) {
+  	return String.fromCharCode.apply(null, new Uint8Array(buf));
+  }
+
+  ext.whenConnected = function() {
+    if (notifyConnection) return true;
+    return false;
+  };
+
+  ext.isSignatureDetected = function() {
+      //console.log("inside xxxx");
+      device.set_receive_handler(function(data) {
+	console.log(ab2str(data));
+      	var inputData = new Uint8Array(data);
+      	console.log(inputData);
+      	console.log(UInt8ArrayToString(inputData));
+      });
+  };
+
+  ext._getStatus = function() {
+    if (!connected)
+      return { status:1, msg:'Disconnected' };
+    else
+      return { status:2, msg:'Connected' };
+  };
+
+  ext._deviceRemoved = function(dev) {
+    console.log('Device removed');
+    // Not currently implemented with serial devices
+  };
+
+  var potentialDevices = [];
+  ext._deviceConnected = function(dev) {
+    potentialDevices.push(dev);
+    if (!device)
+      tryNextDevice();
+  };
+
+  var poller = null;
+  var watchdog = null;
+  function tryNextDevice() {
+    device = potentialDevices.shift();
+    if (!device) return;
+
+    device.open({ stopBits: 0, bitRate: 9600, ctsFlowControl: 0 });
+    console.log('Attempting connection with ' + device.id);
+    /*
+    device.set_receive_handler(function(data) {
+      var inputData = new Uint8Array(data);
+      processInput(inputData);
+    });*/
+
+    /*
+    poller = setInterval(function() {
+      queryFirmware();
+    }, 1000);*/
+
+    watchdog = setTimeout(function() {
+      clearInterval(poller);
+      poller = null;
+      device.set_receive_handler(null);
+      device.close();
+      device = null;
+      tryNextDevice();
+    }, 5000);
+  }
+
+  ext._shutdown = function() {
+    // TODO: Bring all pins down
+    if (device) device.close();
+    if (poller) clearInterval(poller);
+    device = null;
+  };
+
+  // Check for GET param 'lang'
+  var paramString = window.location.search.replace(/^\?|\/$/g, '');
+  var vars = paramString.split("&");
+  var lang = 'en';
+  for (var i=0; i<vars.length; i++) {
+    var pair = vars[i].split('=');
+    if (pair.length > 1 && pair[0]=='lang')
+      lang = pair[1];
+  }
+
+  var blocks = {
+    en: [
+      ['h', 'when device is connected', 'whenConnected'],
+      ['b', '%m.sinatures detected?', 'isSignatureDetected', 'Signature 1']
+    ],
+    zh: [
+      ['h', '當裝置連接時', 'whenConnected'],
+      ['b', '%m.sinatures 检测?', 'isSignatureDetected', '模式 1']
+    ]
+  };
+
+  var menus = {
+    en: {
+      sinatures: ['Signature 1', 'Signature 2', 'Signature 3', 'Signature 4', 'Signature 5', 'Signature 6', 'Signature 7']
+    },
+    zh: {
+      signatures: ['模式 1', '模式 2', '模式 3', '模式 4', '模式 5', '模式 6', '模式 7']
+    }
+  };
+
+  var descriptor = {
+    blocks: blocks[lang],
+    menus: menus[lang],
+    url: 'http://www.acorncodinglab.com/scratch-pixycam-extension'
+  };
+
+  ScratchExtensions.register('AcornCodingLab-PixyCam', descriptor, ext, {type:'serial'});
+
+})({});
